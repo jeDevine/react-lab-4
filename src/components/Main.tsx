4589d955738f8a20e13ed8975ea5ad58e45b70df
james <jamesedwarddevine@gmail.com>
2022-01-19 14:05:08 -0500

    cleaned up

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -11,10 +11,10 @@ const Main = () => {
     useEffect(()=>{
         console.log("b4 useEFF" + search)
         if(search){
-            console.log("search is populated"+search);
+            console.log("search is populated "+search);
             fetchSearchData(search).then(data => setResData(data));
         } else {
-            console.log("search is empty"+search);
+            console.log("search is empty "+search);
             fetchAllData().then(data => setResData(data));
        }
        
